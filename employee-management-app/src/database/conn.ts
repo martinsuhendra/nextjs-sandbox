4944aa1567f829ca37832af219288570b76efd15
martinsuhendra <suhendra.martin90@gmail.com>
2022-08-09 15:54:42 +0700

    refactor: linter for mongo conn & controller

--- employee-management-app/src/database/conn.ts
+++ employee-management-app/src/database/conn.ts
@@ -37,7 +37,7 @@ const connectMongo = async () => {
     cached.conn = await cached.promise
     return cached.conn
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error as string)
   }
 }
 
--- employee-management-app/src/database/controller.ts
+++ employee-management-app/src/database/controller.ts
@@ -1,7 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import Error from 'next/error'
 
-import { EmployeeInput } from '@/app/features/employee/EmployeeForm'
 import Users from '@/app/features/employee/server/model/user'
 
 // GET: http://localhost:3000/api/users
@@ -10,7 +8,7 @@ export async function getUsers(req: NextApiRequest, res: NextApiResponse) {
     const users = await Users.find({})
 
     if (!users) {
-      return res.status(404).json('Error while fetching data!')
+      throw new Error('Error while fetching data!')
     }
     return res.status(200).json(users)
   } catch (error) {
@@ -23,13 +21,13 @@ export async function getUser(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { userId } = req.query
 
-    if (userId) {
-      const user = await Users.findById(userId)
-      res.status(200).json(user)
+    if (!userId) {
+      throw new Error('User not found!')
     }
-    res.status(404).json({ error: 'Data not found' })
-  } catch {
-    res.status(404).json({ error: 'Cannot get the user' })
+    const user = await Users.findById(userId)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({ error })
   }
 }
 
@@ -37,11 +35,11 @@ export async function getUser(req: NextApiRequest, res: NextApiResponse) {
 export async function postUser(req: NextApiRequest, res: NextApiResponse) {
   try {
     const formData = req.body
-    if (!formData)
+    if (!formData) {
       return res.status(404).json({ error: 'Form Data not provided' })
-    Users.create(formData, function (err: Error, data: EmployeeInput) {
-      return res.status(200).json(data)
-    })
+    }
+    const user = await Users.create(formData)
+    return res.status(200).json(user)
   } catch (error) {
     return res.status(404).json({ error })
   }
@@ -57,9 +55,9 @@ export async function putUser(req: NextApiRequest, res: NextApiResponse) {
       const user = await Users.findByIdAndUpdate(userId, formData)
       res.status(200).json(user)
     }
-    return res.status(404).json({ error: 'User not selected' })
-  } catch {
-    return res.status(404).json({ error: 'Error while updating user' })
+    throw new Error('user not selected')
+  } catch (error) {
+    return res.status(404).json({ error })
   }
 }
 
@@ -69,9 +67,10 @@ export async function deleteUser(req: NextApiRequest, res: NextApiResponse) {
     const { userId } = req.query
 
     if (userId) {
+      await Users.findByIdAndDelete(userId)
       res.status(200).json({ deleted: userId })
     }
-    return res.status(404).json({ error: 'User not selected' })
+    throw new Error('User not selected')
   } catch {
     return res.status(404).json({ error: 'Error while deleting user' })
   }
