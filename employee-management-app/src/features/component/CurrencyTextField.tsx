fa5c7a83d4c57e699849e6a7f111540b76d84a09
martinsuhendra <suhendra.martin90@gmail.com>
2022-12-05 11:28:56 +0700

    feat: adding textfield for currency or numeric

--- employee-management-app/src/features/component/CurrencyTextField.tsx
+++ employee-management-app/src/features/component/CurrencyTextField.tsx
@@ -1,123 +1,76 @@
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
+import React from 'react'
 
-import {
-  InputAdornment,
-  OutlinedTextFieldProps,
-  TextField,
-} from '@mui/material'
-import AutoNumeric from 'autonumeric'
+import { InputAdornment, TextField, TextFieldProps } from '@mui/material'
+import { NumericFormat, NumericFormatProps } from 'react-number-format'
 
-interface CurrencyTextFieldProps extends OutlinedTextFieldProps {
-  type?: 'text' | 'tel' | 'hidden'
-  id?: string
-  /** If true, the input element will be disabled. */
-  disabled?: boolean
-  /** The label content. */
-  label: string
-  /** Align the numbers in the textField.
-   * If you pass the `inputProps` from TextFieldAPI text align won't work.
-   * then, you have handle it by className with your own class inside inputProps.
-   *
-  textAlign: "right" | "left" |"center",
-  /** Tab index for the element */
-  tabIndex?: number
-  /** If true, the input element will be focused during the first mount. */
-  autoFocus?: boolean
-  /** The short hint displayed in the input before the user enters a value. */
-  placeholder?: string
-  /** value to be enter and display in input */
-  value?: string | number
-  /** Defines the currency symbol string. */
+type CurrencyTextFieldProps = Omit<TextFieldProps, 'variant'> & {
+  variant?: TextFieldProps['variant']
   currencySymbol: string
-  /** Defines what decimal separator character is used. */
-  decimalCharacter?: string
-  /** Allow to declare an alternative decimal separator which is automatically replaced by `decimalCharacter` when typed. */
-  decimalCharacterAlternative?: string
-  /** Defines the default number of decimal places to show on the formatted value. */
-  decimalPlaces?: number
-  /** Defines how many decimal places should be visible when the element is unfocused null. */
-  decimalPlacesShownOnBlur?: number
-  /** Defines how many decimal places should be visible when the element has the focus. */
-  decimalPlacesShownOnFocus?: number
-  /** Defines the thousand grouping separator character */
-  digitGroupSeparator?: string
-  /** Controls the leading zero behavior */
-  leadingZero?: 'allow' | 'deny' | 'keep'
-  /** maximum value that can be enter */
-  maximumValue?: string
-  /** minimum value that can be enter */
-  minimumValue?: string
-  /** placement of the negitive and possitive sign symbols */
-  negativePositiveSignPlacement?: 'l' | 'r' | 'p' | 's'
-  /** Defines the negative sign symbol to use */
-  negativeSignCharacter?: string
-  /** how the value should be formatted,before storing it */
-  outputFormat?: 'string' | 'number'
-  /** Defines if the element value should be selected on focus. */
-  selectOnFocus?: boolean
-  /** Defines the positive sign symbol to use. */
-  positiveSignCharacter?: string
-  /** Defines if the element should be set as read only on initialization. */
-  readOnly?: boolean
-  /** predefined objects are available in <a href="https://www.nodenpm.com/autonumeric/4.5.1/detail.html#predefined-options">AutoNumeric</a> */
-  preDefined?: object
+  suffix?: string
+  thousandSeparator?: string
+  decimalSeparator?: string
 }
 
-type ChangeEventType =
-  | 'onChange'
-  | 'onFocus'
-  | 'onBlur'
-  | 'onKeyPress'
-  | 'onKeyUp'
-  | 'onKeyDown'
+type NumericFormatCustomProps = {
+  onChange: (event: { target: { name: string; value: string } }) => void
+  name: string
+  suffix?: string
+  thousandSeparator?: string
+  decimalSeparator?: string
+}
+
+const NumericFormatCustom = React.forwardRef<
+  NumericFormatProps,
+  NumericFormatCustomProps
+>((props: NumericFormatCustomProps, ref) => {
+  const {
+    onChange,
+    suffix,
+    thousandSeparator = ',',
+    decimalSeparator = '.',
+    ...other
+  } = props
+  return (
+    <NumericFormat
+      {...other}
+      valueIsNumericString
+      getInputRef={ref}
+      suffix={suffix}
+      thousandSeparator={thousandSeparator}
+      decimalSeparator={decimalSeparator}
+      onValueChange={(values) => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value,
+          },
+        })
+      }}
+    />
+  )
+})
 
 const CurrencyTextField = ({
   currencySymbol,
-  InputProps,
+  variant = 'outlined',
   value,
-  outputFormat = 'number',
+  name,
+  suffix,
+  thousandSeparator,
+  decimalSeparator,
   ...props
 }: CurrencyTextFieldProps) => {
-  const inputRef = useRef('')
-  const [autonumeric, setAutonumeric] = useState<AutoNumeric>()
-
-  const getValue = () => {
-    if (autonumeric) return
-    const valueMapper = {
-      string: (numeric: AutoNumeric) => numeric.getNumericString(),
-      number: (numeric: AutoNumeric) => numeric.getNumber(),
-    }
-    return valueMapper[outputFormat](autonumeric)
-  }
-
-  const callEventHandler = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLDivElement>,
-    eventName: ChangeEventType
-  ) => {
-    if (!props[eventName]) return
-    props[eventName](e, getValue())
-  }
-
-  useEffect(() => {
-    setAutonumeric(new AutoNumeric(inputRef.current, value))
-
-    return () => autonumeric?.remove()
-  }, [])
-
   return (
     <TextField
-      inputRef={inputRef}
-      onChange={(e) => callEventHandler(e, 'onChange')}
-      onFocus={(e) => callEventHandler(e, 'onFocus')}
-      onBlur={(e) => callEventHandler(e, 'onBlur')}
-      onKeyPress={(e) => callEventHandler(e, 'onKeyPress')}
-      onKeyUp={(e) => callEventHandler(e, 'onKeyUp')}
-      onKeyDown={(e) => callEventHandler(e, 'onKeyDown')}
+      variant={variant}
+      value={value}
+      name={name}
       InputProps={{
+        inputComponent: NumericFormatCustom as never,
+        inputProps: { suffix, thousandSeparator, decimalSeparator },
         startAdornment: (
           <InputAdornment position="start">{currencySymbol}</InputAdornment>
         ),
-        ...InputProps,
       }}
       {...props}
     />
--- employee-management-app/src/pages/component/textfield.tsx
+++ employee-management-app/src/pages/component/textfield.tsx
@@ -1,23 +1,82 @@
 import React, { useState } from 'react'
 
-import { Box, Paper, Typography } from '@mui/material'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Box, Button, Paper, Typography } from '@mui/material'
+import { Controller, useForm } from 'react-hook-form'
 
+import yup from '@/common/utils/yupLocale'
 import CurrencyTextField from '@/features/component/CurrencyTextField'
 
+const currencyValidator = yup.object({
+  price: yup.string().required('Price number is required'),
+})
+
+const MINIMUM_VALUE = 1
+const MAXIMUM_VALUE = 1000
+
 const TextFieldPage = () => {
-  const [value, setValue] = useState('')
+  const [inputValue, setinputValue] = useState('')
+  const { control, handleSubmit, setError } = useForm<{ price: string }>({
+    reValidateMode: 'onChange',
+    resolver: yupResolver(currencyValidator),
+    defaultValues: {
+      price: '',
+    },
+  })
+
+  const handleSubmitForm = (values: { price: string }) => {
+    if (+values.price > MAXIMUM_VALUE || +values.price < MINIMUM_VALUE) {
+      return setError('price', {
+        type: 'onChange',
+        message: 'Price number must be within minimum and maximum',
+      })
+    }
+    return alert(`Price is ${values.price}`)
+  }
 
   return (
     <Paper>
-      <Box p={3}>
+      <Box component="form" p={3} onSubmit={handleSubmit(handleSubmitForm)}>
         <Typography sx={{ mb: 3 }}>Currency Textfield</Typography>
         <CurrencyTextField
-          value={value}
+          value={inputValue}
+          name="price"
           label="Price"
-          currencySymbol="S$"
-          variant="outlined"
-          onChange={(e, value) => setValue(value)}
+          currencySymbol="$S"
+          onChange={(e) => setinputValue(e.target.value)}
+          suffix=",00"
         />
+      </Box>
+      <Box component="form" p={3} onSubmit={handleSubmit(handleSubmitForm)}>
+        <Typography sx={{ mb: 3 }}>
+          Currency Textfield w/ React Hook Form
+        </Typography>
+        <Controller
+          name="price"
+          control={control}
+          render={({ field: { value, onChange }, fieldState: { error } }) => {
+            return (
+              <CurrencyTextField
+                required
+                error={!!error}
+                value={value}
+                name="price"
+                label="Price"
+                currencySymbol="$S"
+                onChange={onChange}
+                helperText={error?.message || 'Minimum 1 And Maximum 1000'}
+                suffix=",00"
+              />
+            )
+          }}
+        />
+        <Button
+          sx={{ display: 'block', mt: 3 }}
+          variant="contained"
+          type="submit"
+        >
+          Submit
+        </Button>
       </Box>
     </Paper>
   )
